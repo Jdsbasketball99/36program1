a97692fe0ba4cd5f93877fbe4ef070330fe15bf0
Jdsbasketball99 <72760994+Jdsbasketball99@users.noreply.github.com>
2020-10-13 14:17:30 -0700

    Add files via upload

---
+++ book.cpp
@@ -0,0 +1,5 @@
+#include <string>
+#include "book.h"
+
+using namespace std;
+
---
+++ book.h
@@ -0,0 +1,24 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+
+using namespace std;
+
+
+class book
+{
+    public:
+
+    int ISBN;
+    string bookType;
+    //string bookName;
+
+
+
+    //private:
+
+
+};
+
+#endif
